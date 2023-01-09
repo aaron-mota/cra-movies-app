133e723ff924e13d8074ec6d22b797d70ce578d1
Aaron Motacek <aaronmotacek@gmail.com>
2023-01-08 23:54:35 -0600

    added PROJECTGOALS.md to home page

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,13 +1,50 @@
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
-import { UsersList } from '../users/components/UsersList'
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
+
+
+let md = `# CRUD App (CRA)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Project Goals
+
+- Gain experience using create-react-app (CRA)
+- Gain experience in setting up front end (client) & back end (server) "from scratch"
+- Play around with project architecture (e.g. folders, index.js files, etc.) to improve personal "best practices"
+- Gain experience with Fetch API (previously only used axios)
+
+## End Goal
+
+- Have improved "best practices" for project architecture + have created any related snippets (for future efficiency)
+
+## Minimal-To-No Concern Given To
+
+- UI/UX
+
+## How To
+
+- To swap backends (databases and "routing/database handlers"), uncomment desired backend within \`/server/router/index.js\`
+  - Currently available:
+    - **REST API**
+      - Mock database (local JSON file)
+      - MongoDb
+      - Prisma (wrapping MongoDb)
+    
+  - \`#TODO\`
+    - TypeScript conversions (all options, or at least Prisma options)
+  
+  - No longer attempting, currently
+    - **tRPC API**
+      - tRPC / Prisma+tRPC (too big of an architecture change and too unfamiliar with tRPC... consider again in the future)`
+
 
 export default function Home() {
   return (
     <>
-      <Typography variant="h4" component="h1" sx={{mb: 2}}>Home</Typography>
-
-      <UsersList />
+      <Box ml={4}>
+        <ReactMarkdown children={md} />
+      </Box>
     </>
   )
 }
